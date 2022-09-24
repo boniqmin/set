05948c8a2dcadf5f251aaf8e8eda76402ece2404
Benjamin Oudejans <benjamin.oudejans@gmail.com>
2022-09-24 23:22:09 +0200

    Initial commit of current build

---
+++ src/main.rs
@@ -0,0 +1,263 @@
+mod set;
+use crate::set::{Card, CardSelection, Deck};
+use std::mem;
+use yew::prelude::*;
+// mod rendercards;
+// use crate::rendercards::CardImg;
+
+// #[function_component(App)]
+// fn app() -> Html {
+// }
+
+enum Msg {
+    CardSelected(usize),
+    Reset,
+    Expand,
+}
+
+// fn app() -> Html {
+//     html!{
+//         <>
+//             <Board
+//         </>
+//     }
+// }
+
+#[derive(PartialEq, Properties)]
+struct CardProp {
+    card: Card,
+    selected: bool,
+}
+
+#[function_component(CardImg)]
+fn card_img(card: &CardProp) -> Html {
+    let shapes = if card.card.amount == 0 {
+        html! {shape_svg(&card.card, 0.,0.,1.)}
+    } else if card.card.amount == 1 {
+        html! {  <>
+            {shape_svg(&card.card, 0.,0.,0.5)}
+            {shape_svg(&card.card, 100.,100.,0.5)}
+            </>
+        }
+    } else if card.card.amount == 2 {
+        html! { <>
+            {shape_svg(&card.card, 50.,0.,0.5)}
+            {shape_svg(&card.card, 0.,100.,0.5)}
+            {shape_svg(&card.card, 100.,100.,0.5)}
+            </>
+        }
+    } else {
+        panic!("Invalid amount code")
+    };
+
+    let shadow = if card.selected {
+        "shadow-in"
+    } else {
+        "shadow-out"
+    };
+
+    html! {<svg height={"200"} width={"200"}> // style="background-color:PaleGreen">
+    <rect class={shadow} x="10" y="10" rx="20" ry="20" width="180" height="180"
+    style="fill:WhiteSmoke;stroke:SlateGray;stroke-width:5" />{shapes}</svg>}
+}
+
+fn shape_svg(card: &Card, translate_x: f32, translate_y: f32, scale: f32) -> Html {
+    let color_str = match card.color {
+        0 => "red",
+        1 => "green",
+        2 => "blue",
+        _ => {
+            panic!("Invalid card")
+        }
+    };
+
+    let diag_hatch = html! {
+        <defs>
+        <pattern id="diagonalHatchred" width="10" height="10" patternTransform="rotate(45 0 0)" patternUnits="userSpaceOnUse">
+            <line x1="0" y1="0" x2="0" y2="10" style="stroke:red; stroke-width:4" />
+        </pattern>
+        <pattern id="diagonalHatchgreen" width="10" height="10" patternTransform="rotate(45 0 0)" patternUnits="userSpaceOnUse">
+            <line x1="0" y1="0" x2="0" y2="10" style="stroke:green; stroke-width:4"/>
+        </pattern>
+        <pattern id="diagonalHatchblue" width="10" height="10" patternTransform="rotate(45 0 0)" patternUnits="userSpaceOnUse">
+            <line x1="0" y1="0" x2="0" y2="10" style="stroke:blue; stroke-width:4" />
+        </pattern>
+        </defs>
+    };
+    // let style = format!("fill:{color_str}");
+    let fill = match card.filling {
+        0 => "WhiteSmoke".to_owned(),
+        1 => format!("url(#diagonalHatch{color_str})"),
+        2 => color_str.to_owned(), //format!("{color_str}"),
+        _ => panic!("Invalid filling code"),
+    };
+    let style = format!("stroke-width:5;stroke:{color_str}"); //;{style}");
+    let shape_svg = match card.shape {
+        0 => {
+            html! {<polygon points="100,50 150,150 50,150" stroke-linejoin={"round"} style={style} fill={fill}/>}
+        }
+        1 => {
+            html! {<polygon points="50,50 150,50 150,150 50 150" stroke-linejoin="round" style={style} fill={fill}/>}
+        }
+        2 => html! {<circle cx="100" cy="100" r="50" style={style} fill={fill}/>},
+        _ => {
+            panic!("Invalid shape code")
+        }
+    };
+
+    html! {<g transform={format!("translate({translate_x} {translate_y}) scale({scale} {scale})")}>
+        {diag_hatch} // since this is constant, move somewhere?
+        {shape_svg}
+        // <text x={"100"} y={"100"}>{card.amount+1}</text>
+    </g>}
+}
+
+struct Board {
+    cards: Vec<Card>,
+    deck: Deck,
+    card_selection: CardSelection,
+    num_sets: u32,
+    times_expanded: u32,
+    finished: bool,
+}
+
+impl Board {
+    fn new() -> Self {
+        let mut cards = vec![];
+        let mut deck = Deck::new_shuffled();
+        for _i in 0..12 {
+            cards.push(deck.draw().unwrap()); // unwrap is safe as deck is just created
+        }
+        Self {
+            cards,
+            deck,
+            card_selection: CardSelection::new(),
+            num_sets: 0,
+            times_expanded: 0,
+            finished: false,
+        }
+    }
+
+    fn reset(&mut self) {
+        // let mut cards = vec![];
+        // let mut deck = Deck::new_shuffled();
+        // for _i in 0..12 {
+        //     cards.push(deck.draw().unwrap());
+        // }
+        // self.cards = cards;
+        // self.deck = deck;
+        // self.card_selection = CardSelection::new();
+        // self.num_sets = 0;
+        // self.times_expanded = 0;
+        let _ = mem::replace(self, Board::new()); // not very nice but ok
+    }
+
+    fn expand(&mut self) {
+        for _ in 0..3 {
+            if let Some(card) = self.deck.draw() {
+                // button should be hidden if deck empty, but just to be sure
+                self.cards.push(card);
+            }
+        }
+    }
+}
+
+impl Component for Board {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self::new()
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        let grid = self
+            .cards
+            .iter()
+            .enumerate()
+            .map(move |(i, card)| {
+                // let on_card_select = {
+                //     let on_click = on_click.clone();
+                //     let card = card.clone();
+                //     Callback::from(move |_| on_click.emit(card.clone()))
+                // };
+
+                let on_card_click = _ctx.link().callback(move |_| Msg::CardSelected(i as usize));
+                let class;
+                if self.card_selection.is_selected(i) {
+                    class = "grid-item selected"
+                } else {
+                    class = "grid-item unselected"
+                }
+
+                // html! {<button class={class} onclick={on_card_click}><img src={format!("cards/{card}.png")} alt={format!("{card}")}/></button>}
+                html! {<button class={class} onclick={on_card_click}><CardImg card={card.clone()} selected={self.card_selection.is_selected(i)}/></button>}
+            })
+            .collect::<Html>();
+
+        let reset_onclick = _ctx.link().callback(|_| Msg::Reset);
+        let expand_onclick = _ctx.link().callback(|_| Msg::Expand);
+        // let card0 = self.cards[0];
+        html! {<>
+        <div class="grid-container">{grid}</div>
+            <p>{format!("Number of sets found: {}", self.num_sets)}</p>
+            // <CardImg card={self.cards[0].clone()} />
+            if self.times_expanded < 3 && !self.deck.is_empty() {  // 3 times expanded => 21 cards, so we always have a set (cap set problem)
+                <button onclick={expand_onclick}>{"Expand"}</button>
+            }
+            <button onclick={reset_onclick}>{"Reset"}</button>
+            <p>{self.finished.to_string()}</p>
+            </>}
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        if self.finished {
+            return false;
+        }
+        match msg {
+            Msg::CardSelected(card) => {
+                self.card_selection.add_next_toggle(card).unwrap();
+                if self.card_selection.is_full() {
+                    if self.card_selection.is_set(&self.cards) {
+                        self.num_sets += 1;
+                        if self.times_expanded > 0 {
+                            self.card_selection.remove_cards(&mut self.cards);
+                            self.times_expanded -= 1;
+                        } else {
+                            self.card_selection
+                                .replace_cards_from_deck(&mut self.cards, &mut self.deck);
+                        }
+                        // if self.deck.is_empty() {
+                        //     self.num_sets = 0;
+                        //     self.deck = Deck::new_shuffled();
+                        // }
+                    }
+
+                    self.card_selection.clear();
+                }
+                // if self.deck.is_empty() {
+                //     self.finished = true; // TODO: make screen for when game is finished
+                // }
+
+                if self.cards.is_empty() {
+                    self.finished = true;
+                }
+                true
+            }
+            Msg::Reset => {
+                self.reset();
+                true
+            }
+
+            Msg::Expand => {
+                self.times_expanded += 1;
+                self.expand();
+                true
+            }
+        }
+    }
+}
+
+fn main() {
+    yew::start_app::<Board>();
+}
---
+++ src/set.rs
@@ -0,0 +1,292 @@
+use std::fmt::{Display, Error, Formatter};
+// use yew::prelude::*;
+use rand::seq::SliceRandom;
+
+pub struct CardSelection {
+    card_nums: Vec<Option<usize>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct FullError;
+
+impl CardSelection {
+    pub fn new() -> CardSelection {
+        CardSelection {
+            card_nums: vec![None, None, None],
+        }
+    }
+
+    pub fn is_full(&self) -> bool {
+        for card in self.card_nums.iter() {
+            if card.is_none() {
+                return false;
+            }
+        }
+        true
+    }
+
+    // pub fn add_next(&mut self, next: usize) -> Result<(), FullError> {
+    //     // returns
+    //     for card in self.card_nums.iter_mut() {
+    //         if card.is_none() {
+    //             *card = Some(next);
+    //             return Ok(());
+    //         }
+    //     }
+    //     Err(FullError)
+    // }
+
+    pub fn add_next_toggle(&mut self, next: usize) -> Result<(), FullError> {
+        if self.is_selected(next) {
+            for card in self.card_nums.iter_mut() {
+                // remove all instances of next
+                if card == &Some(next) {
+                    *card = None;
+                }
+            }
+            return Ok(()); // return here to not re-add
+        }
+        for card in self.card_nums.iter_mut() {
+            if card.is_none() {
+                *card = Some(next);
+                return Ok(());
+            }
+        }
+        Err(FullError)
+    }
+
+    pub fn is_set(&self, cards: &Vec<Card>) -> bool {
+        if !self.is_full() {
+            return false;
+        }
+        return is_set(
+            &cards[self.card_nums[0].unwrap()],
+            &cards[self.card_nums[1].unwrap()],
+            &cards[self.card_nums[2].unwrap()],
+        );
+    }
+
+    // pub fn replace_cards_random(&self, cards: &mut Vec<Card>) {
+    //     for card_num in &self.card_nums {
+    //         if let Some(i) = card_num {
+    //             cards[*i] = Card::new_random();
+    //         }
+    //     }
+    // }
+
+    pub fn replace_cards_from_deck(&self, cards: &mut Vec<Card>, deck: &mut Deck) {
+        let mut new_card_opts: Vec<Option<Card>> =
+            cards.iter().map(|card| Some(card.clone())).collect();
+        for card_num in &self.card_nums {
+            if let Some(i) = card_num {
+                let newcard = deck.draw();
+                new_card_opts[*i] = newcard; //newcard.as_ref();
+            }
+        }
+        *cards = new_card_opts
+            .iter()
+            .filter_map(|card| card.clone())
+            .collect();
+
+        // let new_cards = Vec::new();
+        // for card_num in card_nums_sorted {
+        //     if card_num.is_some() {
+        //         match self.deck.draw() {
+        //             Some(card) => new_cards.push(card)
+        //         }
+
+        //     }
+        // }
+    }
+
+    // TODO: make this safe
+    pub fn remove_cards(&self, cards: &mut Vec<Card>) {
+        // for card_num in &self.card_nums {
+        //     if let Some(i) = card_num {
+        //         if i >= 12 { //TODO: magic number
+        //             cards.pop();
+        //         } else {
+        //         cards[*i] = cards.pop().unwrap(); // take from end and reinsert in free spot
+        //     }
+        // }
+        // cards.retain(||)
+        let mut valid_card_nums = self
+            .card_nums
+            .iter()
+            .filter_map(|num| num.as_ref())
+            .collect::<Vec<&usize>>();
+        valid_card_nums.sort_by(|a, b| b.cmp(a));
+        for num in valid_card_nums {
+            if num >= &(cards.len() - 3) {
+                // if it's one of the new cards, just remove it
+                //TODO: magic number
+                cards.pop();
+            } else {
+                // otherwise replace it. Sorting guarantees you never replace by a selected card
+                cards[*num] = cards.pop().unwrap();
+            }
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.card_nums = vec![None, None, None];
+    }
+
+    pub fn is_selected(&self, card_nr: usize) -> bool {
+        self.card_nums.contains(&Some(card_nr))
+    }
+}
+
+impl Display for CardSelection {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        let mut text = String::new();
+        for opt_card in &self.card_nums {
+            let card_repr = match opt_card {
+                None => "-".to_owned(),
+                Some(card) => {
+                    let card = card.clone();
+                    card.to_string()
+                }
+            };
+            text.push_str(&card_repr);
+        }
+        write!(f, "{text} ")
+    }
+}
+
+// fn randint(min: i64, max: i64) -> i64 {
+//     let range = (max - min + 1) as f64;
+//     (rand::random::<f64>() * range).floor() as i64 + min
+// }
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Card {
+    pub shape: u32,
+    pub color: u32,
+    pub filling: u32,
+    pub amount: u32,
+}
+
+impl Card {
+    // pub fn new_random() -> Card {
+    //     Card {
+    //         shape: randint(0, 2) as u32,
+    //         color: randint(0, 2) as u32,
+    //         filling: randint(0, 2) as u32,
+    //         amount: randint(0, 2) as u32,
+    //     }
+    // }
+}
+
+impl Display for Card {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        write!(
+            f,
+            "{}{}{}{}",
+            self.color, self.shape, self.filling, self.amount
+        )
+    }
+}
+
+pub struct Deck {
+    cards: Vec<Card>,
+}
+
+impl Deck {
+    pub fn new() -> Self {
+        let mut cards = Vec::new();
+        for shape in 0..3 {
+            for color in 0..3 {
+                for filling in 0..3 {
+                    for amount in 0..3 {
+                        cards.push(Card {
+                            shape,
+                            color,
+                            filling,
+                            amount,
+                        })
+                    }
+                }
+            }
+        }
+        Deck { cards }
+    }
+
+    pub fn shuffle(&mut self) {
+        self.cards.shuffle(&mut rand::thread_rng());
+    }
+
+    pub fn new_shuffled() -> Self {
+        let mut new = Self::new();
+        new.shuffle();
+        new
+    }
+
+    pub fn draw(&mut self) -> Option<Card> {
+        self.cards.pop()
+    }
+
+    // pub fn peek(&self) -> Option<&Card> {
+    //     self.cards.last()
+    // }
+
+    pub fn is_empty(&self) -> bool {
+        self.cards.is_empty()
+    }
+}
+
+// #[derive(Properties, PartialEq)]
+// pub struct Board {
+//     pub cards: Vec<Card>,
+//     pub on_click: Callback<Card>,
+// }
+
+// impl Board {
+//     // pub fn new_random(size: usize) -> Board {
+//     //     let mut cards = Vec::new();
+//     //     for _ in 0..size {
+//     //         cards.push(Card::new_random())
+//     //     }
+//     //     Board { cards }
+//     // }
+
+//     pub fn replace_random(&mut self, idx: usize) {
+//         self.cards[idx] = Card::new_random();
+//     }
+// }
+
+fn all_same_or_different<T: PartialEq>(first: T, second: T, third: T) -> bool {
+    if first == second && second == third {
+        true
+    } else if first != second && second != third && third != first {
+        true
+    } else {
+        false
+    }
+}
+
+pub fn is_set(first: &Card, second: &Card, third: &Card) -> bool {
+    // true
+    if !all_same_or_different(first.color, second.color, third.color) {
+        false
+    } else if !all_same_or_different(first.shape, second.shape, third.shape) {
+        false
+    } else if !all_same_or_different(first.filling, second.filling, third.filling) {
+        false
+    } else if !all_same_or_different(first.amount, second.amount, third.amount) {
+        false
+    } else {
+        true
+    }
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     #[test]
+//     fn test_rng() {
+//         for _ in 0..8 {
+//             println! {"{}", Card::new_random()}
+//         }
+//     }
+// }
